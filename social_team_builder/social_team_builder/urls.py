931cea8eb29e0944c23ee69fba5a15d4e7106026
ykuruta <yasukokurata@gmail.com>
2018-11-10 18:09:35 -0500

    updated layout.html

--- social_team_builder/social_team_builder/urls.py
+++ social_team_builder/social_team_builder/urls.py
@@ -22,7 +22,7 @@ from . import settings, views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name="home"),
+    url(r'^$', views.Home.as_view(), name="home"),
     url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^projects/', include('projects.urls', namespace="projects")),
