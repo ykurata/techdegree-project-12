8772f8f19037c74719e9b776bc6807330974e1df
ykuruta <yasukokurata@gmail.com>
2018-11-20 23:15:41 -0500

    final commit

--- social_team_builder/accounts/urls.py
+++ social_team_builder/accounts/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    #url(r'signin/$', views.SigninView.as_view(), name="signin"),
     url(r'logout/$', views.LogoutView.as_view(), name="logout"),
     url(r"signup/$", views.SignUp.as_view(), name="signup"),
     url(r"edit/(?P<pk>\d+)/$", views.profile_update, name="edit"),
